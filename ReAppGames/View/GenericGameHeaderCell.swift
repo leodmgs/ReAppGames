e9c2e5bb3db6e924512c32dc77887c5ca064d569
Leonardo Domingues <leodmgs@gmail.com>
2018-12-28 14:47:03 -0300

    Add a generic header cell class

---
+++ ReAppGames/View/GenericGameHeaderCell.swift
@@ -0,0 +1,29 @@
+//
+//  GenericGameHeaderCell.swift
+//  ReAppGames
+//
+//  Created by Leonardo Domingues on 12/28/18.
+//  Copyright © 2018 Leonardo Domingues. All rights reserved.
+//
+
+import UIKit
+
+class GenericGameHeaderCell: UICollectionViewCell {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
